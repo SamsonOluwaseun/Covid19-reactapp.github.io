9cca5b362506c73a5029d1931d0123b89143bf0e
SamsonOluwaseun <39919583+SamsonOluwaseun@users.noreply.github.com>
2020-08-03 02:51:31 +0100

    Updates

---
+++ precache-manifest.75330b0aff96dc0f44734dc250337186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a4b600858058742233b42230f7cb43",
+    "url": "/Covid19-reactapp/index.html"
+  },
+  {
+    "revision": "6d27e5e6fb0a89251eda",
+    "url": "/Covid19-reactapp/static/css/main.d415c85a.chunk.css"
+  },
+  {
+    "revision": "bfbae2edd41860a8c5ef",
+    "url": "/Covid19-reactapp/static/js/2.cd248bd8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-reactapp/static/js/2.cd248bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d27e5e6fb0a89251eda",
+    "url": "/Covid19-reactapp/static/js/main.98aaa558.chunk.js"
+  },
+  {
+    "revision": "005dc7b54c29ead8035e",
+    "url": "/Covid19-reactapp/static/js/runtime-main.3924ddff.js"
+  },
+  {
+    "revision": "5582b802b9638c8d6b3af4098d0ca493",
+    "url": "/Covid19-reactapp/static/media/Covid 19.5582b802.jpg"
+  }
+]);
