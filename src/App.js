49b0abed55032e8a9a93519809b24a3b1f1d707d
SamsonOluwaseun <39919583+SamsonOluwaseun@users.noreply.github.com>
2020-08-03 01:14:38 +0100

    My first comit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Cards from './Components/Cards/Cards';
+// import Chart from './Components/Chart/Chart';
+// import Country from './Components/Country/Country';
+
+import {Cards, Charts,Country} from './Components';// Depending on index.js in components folder
+import style from './App.module.css';
+import {fetchData} from './api';
+import corona from './image/Covid 19.jpg';
+
+
+class App extends React.Component {
+  state= {
+    data:{},
+    country: '',
+
+  }
+  async componentDidMount(){
+    const fetchedData= await fetchData();
+    this.setState({data:fetchedData})
+  }
+  handleCountryChange= async (country) =>{
+    
+    const fetchedData= await fetchData(country);
+    this.setState({data: fetchedData, country: country});
+
+  }
+  render(){
+    const {data,country}= this.state;
+    return(
+        <div className={style.container}> 
+          <img className={style.image} src={corona} alt="COVID"/>
+          <Cards data={data}/>
+          <Country handleCountryChange={this.handleCountryChange}/>
+          <Charts data={data} country = {country}/>
+        </div>
+   );
+}
+}
+
+export default App;
